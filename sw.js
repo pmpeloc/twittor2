d015cd93ed2511ea65c50fac9ecbf5f9f3765b4c
Pablo Misael Peloc <pmpeloc@gmail.com>
2019-01-16 22:21:54 -0300

    Modificacion del SW y CSS

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 const STATIC_CACHE    = 'static-v2';
-const DYNAMIC_CACHE   = 'dynamic-v1';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -40,19 +40,25 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-
+ 
     const respuesta = caches.keys().then( keys => {
-
+ 
         keys.forEach( key => {
-            if ( key !== STATIC_CACHE && key.includes('static') ) {
+ 
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
+ 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+ 
         });
-
+ 
     });
-
+ 
     e.waitUntil( respuesta );
-
+ 
 });
 
 self.addEventListener('fetch', e => {
