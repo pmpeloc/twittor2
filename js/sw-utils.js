59a12237f822dadf68097a66e7e22b230e0e0f3e
Pablo Misael Peloc <pmpeloc@gmail.com>
2019-01-16 21:43:06 -0300

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,18 @@
+// Funcion encargada de guardar el cache dinamico
+
+function actualizarCacheDinamico( dynamicCache, req, res ) {
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+            cache.put( req, res.clone() );
+            return res.clone();
+        });
+
+    } else {
+
+        return res;
+
+    }
+
+}
